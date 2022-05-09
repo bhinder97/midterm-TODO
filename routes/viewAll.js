4834d8b249f8e971d930b29df4995a101d0b8d5b
Nhan <nktran4@gmail.com>
2022-05-09 12:36:55 -0400

    viewAll feature added

---
+++ routes/viewAll.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router  = express.Router();
+
+let queryString = `
+SELECT task
+FROM tasks
+JOIN users ON users.id=users_id
+WHERE users.id = '$1';
+`
+
+module.exports = (db) => {
+  router.post("/viewall", (req, res) => {
+    db.query(queryString, [req.session.user_id])
+    .then(result => {
+      return result.rows;
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ error: err.message });
+    });
+  });
+  return router;
+};
