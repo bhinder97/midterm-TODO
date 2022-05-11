4df23ae702f98424d8bf06e8e3cb1eb3cefc3189
Nhan <nktran4@gmail.com>
2022-05-11 14:40:39 -0400

    code clean up

--- routes/add.js
+++ routes/add.js
@@ -32,26 +32,26 @@ module.exports = (db) => {
     request(`https://www.googleapis.com/books/v1/volumes?q=${req.body.task}`, (error, response, body) => {
         let data = JSON.parse(body);
         if (data.items[0].volumeInfo.readingModes.text === true) {
-          console.log("Test 1:", data.items[0].volumeInfo)
+          // console.log("Test 1:", data.items[0].volumeInfo)
           category = "Books (To read)";
         } else {
           request(`https://www.themealdb.com/api/json/v1/1/search.php?s=${req.body.task}`, (error, response, body) => {
             let data = JSON.parse(body);
             if (data.meals !== null) {
-              console.log("Test 2:", data.meals)
+              // console.log("Test 2:", data.meals)
               category = "Foods (To eat)";
             } else {
               request(`http://www.omdbapi.com/?t=${req.body.task}&apikey=23375eca`, (error, response, body) => {
                 let data = JSON.parse(body);
                 if (data.Year !== null) {
-                  console.log("Test 3:", data.Year)
+                  // console.log("Test 3:", data.Year)
                   category = "Film / Series (To watch)";
                 } else {
                   category = "Products (To buy)";
                 }
                 db.query(queryString, [req.body.users_id, req.body.task, category])
                   .then((result) => {
-                    console.log(result);
+                    // console.log(result);
                     res.redirect("/");
                   })
                   .catch((err) => {
